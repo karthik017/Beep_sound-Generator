b95672b0bb55084e4b91c1fffe370d0ea0e4b591
karthik017 <65389398+karthik017@users.noreply.github.com>
2020-08-09 06:19:28 +0530

    Add files via upload

---
+++ beep_sound.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  9 06:11:12 2020
+
+@author: HP
+"""
+
+import winsound    #module required
+
+fr = 3799         #frequency of the beep sound
+dr = 3000         #duration of the beep sound
+
+winsound.Beep(fr,dr)
